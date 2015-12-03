411d0c14ec4c2862d14814c6c9f20dd1c17a2ea2
shinhra <lecther-one@hotmail.fr>
2015-12-03 13:35:18 +0100

    suppression des valeurs en dure inutiles + fonction generate terminé

--- sudoku.lisp
+++ sudoku.lisp
@@ -1,6 +1,10 @@
-(defvar *tab* (make-array '(9 9)))
-(defvar *playerTab* (make-array '(9 9)))
-(defvar *alpha* '(A B C D E F G H I))
+(defparameter *smallTabSize* 3)
+(defparameter *tabSize* (* *smallTabSize* *smallTabSize*))
+(defparameter *tab* (make-array (make-list 2 :initial-element *tabSize*)))
+(defparameter *playerTab* (make-array (make-list 2 :initial-element *tabSize*)))
+(defparameter *alpha* '(A B C D E F G H I J K L M N O P K R S T U V W X Y Z))
+
+;;############# DRAW ####################################
 
 ;;VERSION RECURSIVE
 
@@ -29,9 +33,9 @@
 ;;    (drawXY 0 0 tab)))
 
 (defun draw-alpha-line(l i)
-  (if(< i 9)
+  (if(< i *tabSize*)
      (let ()
-       (if (= (mod i 3) 0)
+       (if (= (mod i *smallTabSize*) 0)
 	   (format t " ~A " (car l))
 	   (format t "~A " (car l)))
        (draw-alpha-line (cdr l) (1+ i)))
@@ -40,46 +44,48 @@
 (defun draw (tab)
   (format t "  ")
   (draw-alpha-line *alpha* 0)
-  (dotimes (y 9)
+  (dotimes (y *tabSize*)
     (let ()
       (format t "~D " (1+ y)))
-    (dotimes (x 9)
-      (if (= (mod x 3) 0)
+    (dotimes (x *tabSize*)
+      (if (= (mod x *smallTabSize*) 0)
 	  (format t " ~D " (aref tab x y))
 	  (format t "~D " (aref tab x y))))
-    (if (= (mod (+ 1 y) 3) 0)
+    (if (= (mod (+ 1 y) *smallTabSize*) 0)
 	(format t "~%~%")
 	(format t "~%"))))))
 
-(defun generate3x3 (x y)
-  (let ((l '(1 2 3 4 5 6 7 8 9))
-	(dotimes (y 3)
-	  (dotimes (x 3)
-	    (generatePoint x y l))))))
+;;################# GENERATE ###############################
 
-(defun generatePoint (x y l)
-  (dotimes (y 9)
+(defun generate ()
+  (dotimes (y *smallTabSize*)
+    (dotimes (x *smallTabSize*)
+      (generateSmallTab (* x *smallTabSize*) (* y *smallTabSize*)))))
+
+(defun generateSmallTab (i j)
+  (let ((l (makeTabList *tabSize* '())))
+	(dotimes (y *smallTabSize*)
+	  (dotimes (x *smallTabSize*)
+	    (setf l (remove (generatePoint (+ x i) (+ y j) l) l))))))
+
+(defun makeTabList (x l)
+  (if (> x 0)
+      (makeTabList (1- x) (cons x l))
+      l))
+
+(defun generatePoint (i j l)
+  (dotimes (y *tabSize*)
     (if (> 0 (aref *tab* 0 y))
 	(remove (aref *tab* 0 y) l)))
-  (dotimes (x 9)
+  (dotimes (x *tabSize*)
     (if (> 0 (aref *tab* x 0))
 	(remove (aref *tab* x 0) l)))
-  (setf (aref *tab* x y) (nth (random (list-length l)) l)))
-	  
-
-;;avoir une liste (1 à 9) correspondant à un des tableaux 3x3
-;;parcourir en x (le tableau 9x9)
-;;parcourir en y (le tableau 9x9)
-;;en déduire une nouvelle liste qui exclu les valeurs impossibles
-;;faire un random sur cette nouvelle liste
-;;et ajouter la valeur correspondante
-;;supprimer la valeur ajouter de la première liste
-;;recommencer pour tout les tableaux 3x3
+  (setf (aref *tab* i j) (nth (random (list-length l)) l)))
 
 (defun erase (tab)
   (if (equal tab *tab*)
-      (setf *tab* (make-array '(9 9)))
+      (setf *tab* (make-array (make-list 2 :initial-element *tabSize*))))
       (if (equal tab *playerTab*)
-	  (setf *playerTab* (make-array '(9 9))))))
+	  (setf *playerTab* (make-array (make-list 2 :initial-element *tabSize*)))))))
       
   
